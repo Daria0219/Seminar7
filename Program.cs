513196cc5508c5685722cbb1422927e29f84ed70
Your Name <you@example.com>
2022-11-11 20:21:10 +0400

    ДЗ №1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    Console.WriteLine("Введите число строк m");
+    int m=int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число столбцов n");
+    int n=int.Parse(Console.ReadLine());
+
+void FillList(List<List<double>> MyList){
+    for (int i=0; i<m; i++){
+        MyList.Add(new List<double>());
+        for(int j=0; j<n; j++)
+            MyList[i].Add(Math.Round(new Random().NextDouble()*5,1));
+    }
+}
+void PrintList(List<List<double>> MyList){
+    for (int i=0; i<MyList.Count; i++){
+        for(int j=0; j<MyList[i].Count; j++)
+        Console.Write(MyList[i][j]+"  ");
+    Console.WriteLine();}
+    }
+        
+List <List<double>> numbers = new List<List<double>>();
+FillList(numbers);
+PrintList(numbers);
+
